f62bb20dc8106f66ea2792a1724793f31b503e12
Natalia <natalia.arq.123@gmail.com>
2023-01-21 13:56:09 +0100

    Primera subida

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //WriteLine sirve para escribir en consola
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Hola y bienvenidos");
+        }
+    }
+}
